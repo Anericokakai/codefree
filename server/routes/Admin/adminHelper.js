2316d4985fe1eaeb12cb7fec986886792eba978d
dev-kakai <devkakai@localhost.localdomain>
2023-05-30 11:52:32 +0300

    changed the commit for backed delete image

--- server/routes/Admin/adminHelper.js
+++ server/routes/Admin/adminHelper.js
@@ -1,12 +1,17 @@
 import fs from 'fs'
-export const deleteFile=async(image,collection,id,res)=>{
-    
-         
+export const deleteFile=(image,collection,id,res)=>{
+    fs.unlink(image, async (err) => {
+         if (err) {
+          res.json({
+            status: 400,
+            result: "an error occured while deleteing the the file",
+          });
+        } else {
           const delete_git_blog = await collection.findByIdAndDelete(id);
           if (!delete_git_blog)
             return res.json({ status: 400, result: "failed to delete blog" });
           res.json({ status: 200, result: "blog deleted successfully" });
     
-        
-      ;
+        }
+      });
 }
