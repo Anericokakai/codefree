3a2ec2a74d1360f1e4ebf1b83ba9c8b9666c9aa7
dev-kakai <devkakai@localhost.localdomain>
2023-05-30 12:01:50 +0300

    Revert "change server connection"
    This reverts commit 828196af252098a23dbecb9d1f7cb7e7d460ae7b.

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
-
+import {MongoClient,ServerApiVersion}from 'mongodb'
 import * as dotenv from "dotenv";
-dotenv.config();
-const mongourl = process.env.MONGO_DB_URL;
+dotenv.config()
+const mongourl=process.env.MONGO_DB_URL
 
 const connection = async () => {
-  const connect = await mongoose.connect(mongourl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-  });
-  return connect;
+
+  const client=new MongoClient(mongourl,{
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  })
+  
+  const connect= await (await client.connect())
+    return connect
 };
 
 export default connection;
