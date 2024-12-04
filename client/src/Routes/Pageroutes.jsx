51b81e2d784bf61ab002411aeb16ecea2b836fdb
Anericokakai <challykiddoh@gmail.com>
2024-12-04 20:58:31 +0300

    fix some

--- client/src/Routes/Pageroutes.jsx
+++ client/src/Routes/Pageroutes.jsx
@@ -4,23 +4,23 @@ import BlogCard from "../features/Blogs/Components/BlogCard";
 import { ReadBlog } from "../features/Blogs";
 
 const generalroutes = [
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/blogs",
-    element: <Blogs />,
-    children: [
-      {
-        path: "/blogs/:blog_id",
-        element: <BlogCard />,
-      },
-    ],
-  },
-  {
-    path: "/blogs/:blog_id/:id",
-    element: <ReadBlog />,
-  },
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/blogs",
+        element: <Blogs />,
+        children: [
+            {
+                path: "/blogs/:blog_id",
+                element: <BlogCard />,
+            },
+        ],
+    },
+    {
+        path: "/blogs/:blog_id/:id",
+        element: <ReadBlog />,
+    },
 ];
 export default generalroutes;
